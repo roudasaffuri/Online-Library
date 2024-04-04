e0302b90bf3b8e322a6d392857ee9889bda5dfc4
ElieSbeat95 <elie.sbeat95@gmai.com>
2024-04-04 12:39:51 +0300

    Update4/4

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  name: string = '';
+  names: string[] = [];
 
+  addName() {
+    if (this.name !== '') {
+      this.names.push(this.name);
+      this.name = '';
+    }
+    if (this.names.length === 2) {
+      alert('You have entered two names. You cannot enter more names.');
+}
+}
+isValidName(name: string): boolean {
+  const pattern = /^[a-zA-Z ]*$/;
+  return pattern.test(name);//The test() method returns true if it finds a match
+}
 }
